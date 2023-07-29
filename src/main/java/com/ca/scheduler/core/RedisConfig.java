89b09e8e59602d95a6e2698440c620077118d622
Pugazhendhi Thanikasalam <osacn_p.thanikasalam@krungthai.com>
2023-07-29 23:23:12 +0530

    ass

---
+++ src/main/java/com/ca/scheduler/core/RedisConfig.java
@@ -0,0 +1,55 @@
+package com.ca.scheduler.core;
+
+import io.lettuce.core.ClientOptions;
+import io.lettuce.core.SocketOptions;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.AutoConfigureAfter;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
+
+@Configuration
+@AutoConfigureAfter(RedisAutoConfiguration.class)
+public class RedisConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+    @Value("${spring.redis.password}")
+    private String redisPassword;
+
+    @Bean
+    public LettuceConnectionFactory redisConnectionFactory() {
+        RedisStandaloneConfiguration redisConfiguration = new RedisStandaloneConfiguration(redisHost, redisPort);
+        redisConfiguration.setPassword(redisPassword);
+
+        LettuceClientConfiguration clientConfiguration = LettuceClientConfiguration.builder()
+                .commandTimeout(Duration.ofSeconds(10)) // Customize any other Lettuce client options here
+                .clientOptions(ClientOptions.builder()
+                        .socketOptions(SocketOptions.builder().connectTimeout(Duration.ofSeconds(5)).build())
+                        .build())
+                .build();
+        return new LettuceConnectionFactory(redisConfiguration, clientConfiguration);
+    }
+
+    @Bean
+    public RedisTemplate<String, String> redisTemplate() {
+        RedisTemplate<String, String> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory());
+        template.setKeySerializer(new StringRedisSerializer());
+        template.setValueSerializer(new StringRedisSerializer());
+        return template;
+    }
+
+}
--- src/main/java/com/ca/scheduler/core/service/DiscountAccountLimitResetService.java
+++ src/main/java/com/ca/scheduler/core/service/DiscountAccountLimitResetService.java
@@ -7,12 +7,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.HmacUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 @Component
 @Slf4j
@@ -30,11 +34,17 @@ public class DiscountAccountLimitResetService {
     @Value("${account.key}")
     private String accountKey;
 
+    @Autowired
+    private RedisTemplate<String, String> redisTemplate;
+
     @Scheduled(cron="0 0 0 1 1/1 *")
+    @Transactional
     public void process() {
         log.info("====================================START==========================================");
         log.info("DiscountAccountLimitResetService task started at :  "+ LocalDateTime.now());
 
+        redisTemplate.opsForValue().set("MONTHLY::BATCH","TRUE",15, TimeUnit.MINUTES);
+        log.info("BEFORE DELETE MONTHLY::BATCH VALUE  : "+redisTemplate.opsForValue().get("MONTHLY::BATCH"));
         Optional<SubsidyProduct> subsidyProduct = subsidyProductRepository.findBySubsidyCode(subsidyCode);
         if(!subsidyProduct.isPresent()){
             log.error("Subsidy product not  found : "+subsidyCode);
@@ -43,6 +53,7 @@ public class DiscountAccountLimitResetService {
         List<SubsidyAccountBalance> subsidyAccountBalanceList = subsidyAccountBalanceRepository.findAll();
         if(subsidyAccountBalanceList != null){
             for(SubsidyAccountBalance subsidyAccountBalance : subsidyAccountBalanceList){
+                log.info("before update price reset by batch : "+subsidyAccountBalance);
                 if(subsidyAccountBalance.getCurrentResetAt() != null && subsidyAccountBalance.getNextResetAt() != null
                         && LocalDateTime.now().isAfter(subsidyAccountBalance.getNextResetAt()) && subsidyAccountBalance.getCurrentResetAt().isBefore(subsidyAccountBalance.getNextResetAt())) {
                     subsidyAccountBalance.setAvailableLiter(subsidyAccountBalance.getAllocatedLiter());
@@ -52,13 +63,14 @@ public class DiscountAccountLimitResetService {
                     subsidyAccountBalance.setNextResetAt(subsidyAccountBalance.getNextResetAt().plusMonths(1));
                     subsidyAccountBalance.setSecureHash(computeHash(subsidyAccountBalance.getAvailableLiter()+""+subsidyAccountBalance.getAccount()));
                     subsidyAccountBalanceRepository.save(subsidyAccountBalance);
-                    log.error("Updated Price Reset By Batch : "+subsidyAccountBalance);
+                    log.info("After update price reset by batch : "+subsidyAccountBalance);
                 }
             }
         }
         log.info("DiscountAccountLimitResetService task ended at :  "+ LocalDateTime.now());
         log.info("***********************************END*********************************************");
-
+        redisTemplate.delete("MONTHLY::BATCH");
+        log.info("AFTER DELETE MONTHLY::BATCH VALUE : "+redisTemplate.opsForValue().get("MONTHLY::BATCH"));
     }
 
     public String computeHash(String data) {
@@ -68,4 +80,5 @@ public class DiscountAccountLimitResetService {
         log.info("hmac Value  : "+hmac);
         return hmac;
     }
+
 }
