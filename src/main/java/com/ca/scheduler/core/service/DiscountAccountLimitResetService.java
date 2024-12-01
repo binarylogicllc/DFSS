89966f9dccda5e0de613687efb231d35f6de4d05
Pugazhendhi Thanikasalam <osacn_p.thanikasalam@krungthai.com>
2024-12-01 12:06:02 +0530

    add

--- src/main/java/com/ca/scheduler/core/service/DiscountAccountLimitResetService.java
+++ src/main/java/com/ca/scheduler/core/service/DiscountAccountLimitResetService.java
@@ -7,6 +7,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.HmacUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -37,37 +38,108 @@ public class DiscountAccountLimitResetService {
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
 
-    @Scheduled(cron="0 0 0 1 1/1 *")
+    @PostConstruct
+    public void runImmediately() {
+        processImmediately();
+    }
+
     @Transactional
+    @Scheduled(cron = "0 0 0 1 1/1 *")
     public void process() {
         log.info("====================================START==========================================");
-        log.info("DiscountAccountLimitResetService task started at :  "+ LocalDateTime.now());
+        log.info("DiscountAccountLimitResetService task started at: " + LocalDateTime.now());
 
-        redisTemplate.opsForValue().set("MONTHLY::BATCH","TRUE",15, TimeUnit.MINUTES);
-        log.info("BEFORE DELETE MONTHLY::BATCH VALUE  : "+redisTemplate.opsForValue().get("MONTHLY::BATCH"));
+        redisTemplate.opsForValue().set("MONTHLY::BATCH", "TRUE", 15, TimeUnit.MINUTES);
+        log.info("BEFORE DELETE MONTHLY::BATCH VALUE: " + redisTemplate.opsForValue().get("MONTHLY::BATCH"));
+
         Optional<SubsidyProduct> subsidyProduct = subsidyProductRepository.findBySubsidyCode(subsidyCode);
-        if(!subsidyProduct.isPresent()){
-            log.error("Subsidy product not  found : "+subsidyCode);
+        if (!subsidyProduct.isPresent()) {
+            log.error("Subsidy product not found: " + subsidyCode);
             return;
         }
-        List<SubsidyAccountBalance> subsidyAccountBalanceList = subsidyAccountBalanceRepository.findAll();
-        if(subsidyAccountBalanceList != null){
-            for(SubsidyAccountBalance subsidyAccountBalance : subsidyAccountBalanceList){
-                    log.info("before update price reset by batch : "+subsidyAccountBalance);
+
+        int batchSize = 1000;
+        int page = 0;
+
+        while (true) {
+            log.info("Processing batch page: " + page);
+            var subsidyAccountBalances = subsidyAccountBalanceRepository.findAll(PageRequest.of(page, batchSize));
+            if (!subsidyAccountBalances.hasContent()) {
+                break;
+            } else {
+                log.info("Total records in this batch: " + subsidyAccountBalances.getNumberOfElements());
+            }
+
+            subsidyAccountBalances.forEach(subsidyAccountBalance -> {
+                log.info("Before update: " + subsidyAccountBalance);
+                subsidyAccountBalance.setAvailableLiter(subsidyAccountBalance.getAllocatedLiter());
+                subsidyAccountBalance.setUpdatedAt(LocalDateTime.now());
+                subsidyAccountBalance.setUpdatedBy("Batch");
+                subsidyAccountBalance.setCurrentResetAt(LocalDateTime.now());
+                subsidyAccountBalance.setNextResetAt(subsidyAccountBalance.getNextResetAt().plusMonths(1));
+                subsidyAccountBalance.setSecureHash(computeHash(subsidyAccountBalance.getAvailableLiter() + "" + subsidyAccountBalance.getAccount()));
+                subsidyAccountBalanceRepository.save(subsidyAccountBalance);
+                log.info("After update: " + subsidyAccountBalance);
+            });
+            page++;
+        }
+
+        log.info("DiscountAccountLimitResetService task ended at: " + LocalDateTime.now());
+        log.info("***********************************END*********************************************");
+        redisTemplate.delete("MONTHLY::BATCH");
+        log.info("AFTER DELETE MONTHLY::BATCH VALUE: " + redisTemplate.opsForValue().get("MONTHLY::BATCH"));
+    }
+
+
+
+    @Transactional
+    public void processImmediately() {
+        log.info("====================================START==========================================");
+        log.info("LimitResetService task started at :  " + LocalDateTime.now());
+
+        Optional<SubsidyProduct> subsidyProduct = subsidyProductRepository.findBySubsidyCode(subsidyCode);
+        if (!subsidyProduct.isPresent()) {
+            log.error("Subsidy product not found: " + subsidyCode);
+            return;
+        }
+
+        int batchSize = 1000; // Number of records to process per batch
+        int page = 0; // Start with the first page
+
+        while (true) {
+            log.info("Processing batch page: " + page);
+
+            // Fetch records in batches
+            var subsidyAccountBalances = subsidyAccountBalanceRepository.findAll(PageRequest.of(page, batchSize));
+            if (!subsidyAccountBalances.hasContent()) {
+                break;
+            }else{
+                log.info("Total records in this batch : "+subsidyAccountBalances.getNumberOfElements());
+            }
+
+            // Process each record in the batch
+            subsidyAccountBalances.forEach(subsidyAccountBalance -> {
+                log.info("Before LimitResetService Update  : " + subsidyAccountBalance);
+                if (subsidyAccountBalance.getCurrentResetAt() != null && subsidyAccountBalance.getNextResetAt() != null &&
+                        subsidyAccountBalance.getCurrentResetAt().isBefore(subsidyAccountBalance.getNextResetAt())
+                        && subsidyAccountBalance.getNextResetAt().isBefore(LocalDateTime.now())) {
+                    log.info("Resetting the limit for this account : " + subsidyAccountBalance.getAccount());
                     subsidyAccountBalance.setAvailableLiter(subsidyAccountBalance.getAllocatedLiter());
                     subsidyAccountBalance.setUpdatedAt(LocalDateTime.now());
                     subsidyAccountBalance.setUpdatedBy("Batch");
                     subsidyAccountBalance.setCurrentResetAt(LocalDateTime.now());
                     subsidyAccountBalance.setNextResetAt(subsidyAccountBalance.getNextResetAt().plusMonths(1));
-                    subsidyAccountBalance.setSecureHash(computeHash(subsidyAccountBalance.getAvailableLiter()+""+subsidyAccountBalance.getAccount()));
+                    subsidyAccountBalance.setSecureHash(computeHash(subsidyAccountBalance.getAvailableLiter() + "" + subsidyAccountBalance.getAccount()));
                     subsidyAccountBalanceRepository.save(subsidyAccountBalance);
-                    log.info("After update price reset by batch : "+subsidyAccountBalance);
-            }
+                    log.info("After LimitResetService Update : " + subsidyAccountBalance);
+                } else {
+                    log.info("No need to reset the limit for this account : " + subsidyAccountBalance.getAccount());
+                }
+            });
+            page++;
         }
-        log.info("DiscountAccountLimitResetService task ended at :  "+ LocalDateTime.now());
+        log.info("LimitResetService task ended at :  " + LocalDateTime.now());
         log.info("***********************************END*********************************************");
-        redisTemplate.delete("MONTHLY::BATCH");
-        log.info("AFTER DELETE MONTHLY::BATCH VALUE : "+redisTemplate.opsForValue().get("MONTHLY::BATCH"));
     }
 
     public String computeHash(String data) {
