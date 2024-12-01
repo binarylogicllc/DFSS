e2c2778fc96f7567669656608770dbb3b06878c2
Pugazhendhi Thanikasalam <osacn_p.thanikasalam@krungthai.com>
2024-12-01 13:15:34 +0530

    add

--- src/main/java/com/ca/scheduler/core/dao/SubsidyAccountBalanceRepository.java
+++ src/main/java/com/ca/scheduler/core/dao/SubsidyAccountBalanceRepository.java
@@ -3,6 +3,7 @@ package com.ca.scheduler.core.dao;
 import com.ca.scheduler.core.domain.SubsidyAccountBalance;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalDateTime;
@@ -13,6 +14,6 @@ public interface SubsidyAccountBalanceRepository extends JpaRepository<SubsidyAc
 
     Optional<SubsidyAccountBalance> findByAccount(String account);
 
-    Page<SubsidyAccountBalance> findByNextResetAtBefore(LocalDateTime nextResetAt, PageRequest pageRequest);
+    Page<SubsidyAccountBalance> findByNextResetAtBefore(LocalDateTime nextResetAt, Pageable pageable);
 
 }
